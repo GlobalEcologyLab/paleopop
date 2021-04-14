d2bd0b5eef4f47787e81bc7ffbbbb82ef3e43e80
Julia Pilowsky <japilo@users.noreply.github.com>
2021-04-14 16:21:28 +0930

    Added tests for region_subset function

---
+++ tests/testthat/test-region_subset.R
@@ -0,0 +1,19 @@
+test_that("subset range", {
+  coordinates <- data.frame(x = rep(seq(-1, -5, -1), 5),
+                            y = rep(seq(1, 5, 1), each = 5))
+  values <- matrix(seq(1, 25, 1))
+  subset <- data.frame(x = rep(seq(0, -4, -0.5), 5), 
+                                 y = rep(seq(1, 3, 0.5), each = 9))
+  rsub <- region_subset(coordinates, values, subset)
+  expect_true(all(sort(unique(dplyr::semi_join(cbind(coordinates, values), subset, by = c("x", "y"))$values))==sort(unique(rsub))))
+})
+
+test_that("subset interpolation", {
+  coordinates <- data.frame(x = rep(seq(-1, -5, -1), 5),
+                            y = rep(seq(1, 5, 1), each = 5))
+  values <- matrix(seq(1, 25, 1))
+  subset <- data.frame(x = rep(seq(0, -4, -0.5), 5), 
+                       y = rep(seq(1, 3, 0.5), each = 9))
+  rsub <- region_subset(coordinates, values, subset)
+  expect_length(rsub, nrow(subset))
+})
